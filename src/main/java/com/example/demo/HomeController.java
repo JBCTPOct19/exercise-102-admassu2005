f328d546ed735ed829179822aa7f6a6c2d4d0be4
Admaassu <admassu2005@gmail.com>
2019-10-08 11:54:18 -0400

    first commit

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,4 +1,17 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+    @RequestMapping("/")
+    public String HomePage(Model model){
+        model.addAttribute("myvar", "Say hello to the people.");
+        model.addAttribute("myvar1","Say how are you to the people.");
+
+        return "Admassu_2";
+
+    }
 }
